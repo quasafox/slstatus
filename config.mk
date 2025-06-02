70c809d386807c29bf672e9ce4401b27bf9c887d
Kacper Jan <quasafox@posteo.net>
2025-06-02 15:52:47 +0200

    config.mk: Add -O2 -flto to LDFLAGS

--- config.mk
+++ config.mk
@@ -10,7 +10,7 @@ MANPREFIX = $(PREFIX)/share/man
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_GNU_SOURCE -DVERSION=\"${VERSION}\"
 CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Wno-unused-parameter -O2 -flto
-LDFLAGS  = -s
+LDFLAGS  = -s -O2 -flto
 # OpenBSD: add -lsndio
 # FreeBSD: add -lkvm -lsndio
 LDLIBS   =
